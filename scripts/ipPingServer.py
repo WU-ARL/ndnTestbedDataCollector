e8356b609948fd8feb511cdcdfd2e27bbafde6ba
jdehart1 <jdd@wustl.edu>
2015-09-17 13:35:38 -0500

    working on Testbed

--- scripts/ipPingServer.py
+++ scripts/ipPingServer.py
@@ -2,30 +2,34 @@
 
 import time
 import BaseHTTPServer
-
-import process_topology as topo
-
 import os
 import time
 import sys
 #import html
 #from terminaltables import AsciiTable
+import process_topology as topo
+
+links_file_name = ""
+if topo.RUN_IN_ONL == 1:
+  links_file_name = '../linksList'
+else:
+  links_file_name = '../linksList.testbed'
 
 # Each
-HOST_NAME = '192.168.21.1' # !!!REMEMBER TO CHANGE THIS!!!
-PORT_NUMBER = 80
+HOST_NAME = '128.252.153.28' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 8080
 
 #key format: /name/prefix-192.168.1.1 (neighbor IP address)
 key_id = dict()
 id_key = dict()
-id_rtt = [0]
+id_rtt = dict()
 table_data = [["Link ID", "Prefix-IP Pair", "RTT (ms)"]]
 
 update = 0
 last_update = 0
 
 def populate_id_rtt_table():
-  f = open('linksList', 'r')
+  f = open(links_file_name, 'r')
   for line in f:
     line = line.rstrip()
     comps = line.split(" ")
@@ -33,26 +37,30 @@ def populate_id_rtt_table():
     link_id = comps[0]
     key_id[key] = link_id
     id_key[int(link_id)] = key
-    id_rtt.append(0)
+    id_rtt[int(link_id)] = 0
 
 def dump_rtt():
   processed = dict()
-  for link_id in range(1, len(id_rtt)):
+  #for link_id in range(1, len(id_rtt)):
+  for link_id in id_rtt:
     key = id_key[link_id]
     if key not in processed:
       one_line = []
       one_line.append("{:<3}".format(str(link_id)))
       one_line.append("{:<20}".format(id_key[link_id].upper()))
       one_line.append("{:<10}".format(str(id_rtt[link_id])))
-			
-      r_key = key.split(" --> ")[1] + " --> " + key.split(" --> ")[0]
-      r_id = int(key_id[r_key])
-      one_line.append("{:<3}".format(str(r_id)))
-      one_line.append("{:<20}".format(id_key[r_id].upper()))
-      one_line.append("{:<10}".format(str(id_rtt[r_id])))
+
+      #r_key = key.split(" --> ")[1] + " --> " + key.split(" --> ")[0]
+      #r_id = int(key_id[r_key])
+      #one_line.append("{:<3}".format(str(r_id)))
+      #one_line.append("{:<20}".format(id_key[r_id].upper()))
+      #one_line.append("{:<10}".format(str(id_rtt[r_id])))
 
       processed[key] = 1
-      processed[r_key] = 1
+      #processed[r_key] = 1
+
+      id_rtt[link_id] = 0
+      #id_rtt[r_id] = 0
 
       print one_line
       #table_data.append(one_line)
@@ -62,7 +70,7 @@ def dump_rtt():
 def dump_rtt_html():
   table_code = ""
   table_code += '<table>\n'
-  for link_id in range(1, len(id_rtt)):
+  for link_id in id_rtt:
     table_code += '<tr>\n'
     table_code += '<td>'+str(link_id)+'</td><td>'+id_key[link_id]+'</td><td>'+str(id_rtt[link_id])+'</td>'+"\n"
     table_code += '</tr>\n'
@@ -96,16 +104,19 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
         key = key_0 + " --> " + key_1
         link_id = key_id[key]
+        #print "link_id: ", link_id, " key: ", key
         id_rtt[int(link_id)] = rtt
 
       global update
       update = update + 1
       if update > 100:
+      #if update > 10:
         os.system('clear')
         dump_rtt()
         update = 0
 
-    if "/get_ip_rtt.html" == s.path:
+    if "/get_ip_rtt.html" in s.path:
+      print "HELLO HAOWEI"
       s.send_response(200)
       s.send_header("Content-type", "text/html")
       s.end_headers()
