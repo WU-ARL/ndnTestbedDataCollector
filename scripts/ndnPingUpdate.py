79724e1088472b1aa99fb61d9597f921d1210c0a
jdehart1 <jdd@wustl.edu>
2015-09-17 15:13:33 -0500

    fix

--- scripts/ndnPingUpdate.py
+++ scripts/ndnPingUpdate.py
@@ -19,11 +19,11 @@ proc_list = []
 
 for dest_ip_prefix in n:
   destination = ip_prefix2site_prefix[dest_ip_prefix]
-  cmd = "ndnping -c 3 " +  destination
+  cmd = "ndnping -c 1 " +  destination
 
   proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
   (out, err) = proc.communicate()
-  if 'Avg' in out:
+  if 'avg' in out:
     out = out.split("/")
     result += site + ":" + dest_ip_prefix + "+" + out[-2] + "&"
 
