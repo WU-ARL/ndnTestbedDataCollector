88fabdc21603c4146e15a62b6e92df6bc25ab049
WU-ARL <jdd@arl.wustl.edu>
2015-09-19 07:51:06 -0600

    updated getLoad.sh

--- scripts/getLoad.sh
+++ scripts/getLoad.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-load=`w | head -1`
-echo $load
+DATE=`date`
+LOAD=`w | head -1`
+#TOP=`top -n 1 | grep "PID\|nfd" | grep -v nfdstat`
+#TOP=`top -n 1 | grep "nfd" | grep -v nfdstat`
+PS=`ps alx | grep "PID\|nfd" | grep "PID\|config" | grep -v grep`
+#echo "PID   USER PR NI VIRT  RES SHR S  %CPU  %MEM  TIME+   COMMAND"
+echo ""
+echo "$DATE"
+echo ""
+#echo "$TOP"
+echo '> ps alx | grep "PID\|nfd" | grep "PID\|config" | grep -v grep'
+echo ""
+echo "$PS"
+echo ""
+echo "> w | head -1"
+echo "$LOAD"
