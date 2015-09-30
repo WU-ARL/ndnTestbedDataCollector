d15457781c00b1a228ce937260913947a7670e88
jdehart1 <jdd@wustl.edu>
2015-09-30 08:51:25 -0500

    start Testbed collection

---
+++ Testbed/testAllCpuMem.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------"
+echo "Node BASEL:"
+build/nfdDataCollectorCmd  -i /ndn/ch/unibas -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node BUPT:"
+build/nfdDataCollectorCmd  -i /ndn/cn/edu/bupt -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node TONGJI:"
+build/nfdDataCollectorCmd  -i /ndn/cn/edu/tongji -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node ORANGE:"
+build/nfdDataCollectorCmd  -i /ndn/com/orange -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node ARIZONA:"
+build/nfdDataCollectorCmd  -i /ndn/edu/arizona -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node BYU:"
+build/nfdDataCollectorCmd  -i /ndn/edu/byu -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node CSU:"
+build/nfdDataCollectorCmd  -i /ndn/edu/colostate -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node ILLINOIS:"
+build/nfdDataCollectorCmd  -i /ndn/edu/illinois -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node MEMPHIS:"
+build/nfdDataCollectorCmd  -i /ndn/edu/memphis -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node NEU:"
+build/nfdDataCollectorCmd  -i /ndn/edu/neu -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node UCI:"
+build/nfdDataCollectorCmd  -i /ndn/edu/uci -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node UCLA:"
+build/nfdDataCollectorCmd  -i /ndn/edu/ucla -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node REMAP:"
+build/nfdDataCollectorCmd  -i /ndn/edu/ucla/remap -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node MICHIGAN:"
+build/nfdDataCollectorCmd  -i /ndn/edu/umich -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node WU:"
+build/nfdDataCollectorCmd  -i /ndn/edu/wustl -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node URJC:"
+build/nfdDataCollectorCmd  -i /ndn/es/urjc -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node SYSTEMX:"
+build/nfdDataCollectorCmd  -i /ndn/fr/irt-systemx -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node LIP6:"
+build/nfdDataCollectorCmd  -i /ndn/fr/lip6 -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node UNIPD:"
+build/nfdDataCollectorCmd  -i /ndn/it/unipd -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node WASEDA:"
+build/nfdDataCollectorCmd  -i /ndn/jp/waseda -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node ANYANG:"
+build/nfdDataCollectorCmd  -i /ndn/kr/anyang -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node KISTI:"
+build/nfdDataCollectorCmd  -i /ndn/kr/re/kisti -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
+echo "Node CAIDA:"
+build/nfdDataCollectorCmd  -i /ndn/org/caida -y getCpuMemCfg.sh
+echo "-----------------------------------------------------------"
