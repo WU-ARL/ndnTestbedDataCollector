3f791576b465c7939d8b705a8f2cc31fc01ff854
jdehart1 <jdd@wustl.edu>
2015-10-05 02:16:25 +0900

    fix ps command

--- scripts/getPerfData.sh
+++ scripts/getPerfData.sh
@@ -4,7 +4,7 @@ TIME=`date +%s.%N`
 LOAD=`w | head -1`
 #TOP=`top -n 1 | grep "PID\|nfd" | grep -v nfdstat`
 #TOP=`top -n 1 | grep "nfd" | grep -v nfdstat`
-NFD_PID=`ps aux | grep nfd | grep conf | tr -s ' ' | cut -d ' ' -f 2`
+NFD_PID=`ps aux | grep nfd | grep config | tr -s ' ' | cut -d ' ' -f 2`
 PS=`ps -p $NFD_PID -o comm,pid,%cpu,%mem,rss,vsz,time`
 #PS=`ps alx | grep "PID\|nfd" | grep "PID\|config" | grep -v grep`
 #echo "PID   USER PR NI VIRT  RES SHR S  %CPU  %MEM  TIME+   COMMAND"
