1303a8b872407d2492efa1b25a2a6be8de3adc35
jdehart1 <jdd@wustl.edu>
2015-10-04 12:08:51 -0500

    change perf data to include %cpu and %mem

--- scripts/getPerfData.sh
+++ scripts/getPerfData.sh
@@ -4,14 +4,17 @@ TIME=`date +%s.%N`
 LOAD=`w | head -1`
 #TOP=`top -n 1 | grep "PID\|nfd" | grep -v nfdstat`
 #TOP=`top -n 1 | grep "nfd" | grep -v nfdstat`
-PS=`ps alx | grep "PID\|nfd" | grep "PID\|config" | grep -v grep`
+NFD_PID=`ps aux | grep nfd | grep conf | tr -s ' ' | cut -d ' ' -f 2`
+PS=`ps -p $NFD_PID -o comm,pid,%cpu,%mem,rss,vsz,time`
+#PS=`ps alx | grep "PID\|nfd" | grep "PID\|config" | grep -v grep`
 #echo "PID   USER PR NI VIRT  RES SHR S  %CPU  %MEM  TIME+   COMMAND"
 echo ""
 echo "$DATE"
 echo "TIME $TIME"
 echo ""
 #echo "$TOP"
-echo '> ps alx | grep "PID\|nfd" | grep "PID\|config" | grep -v grep'
+echo '> ps -p $NFD_PID -o comm,pid,%cpu,%mem,rss,vsz,time'
+#echo '> ps alx | grep "PID\|nfd" | grep "PID\|config" | grep -v grep'
 echo ""
 echo "$PS"
 echo ""
