200484dd47cf75f40b3b81888cefeed693a5cfe6
jdehart1 <jdd@wustl.edu>
2016-02-29 16:14:51 -0600

    expand monitor

--- NDNRTC/monitorGW.sh
+++ NDNRTC/monitorGW.sh
@@ -9,24 +9,37 @@ else
   echo "Example: $0 WU /ndn/edu/wustl"
   exit 0
 fi
+CPU_TYPE=`/home/jdd/WU-ARL/ndn-ops/NOC/nfdDataCollection/build/nfdDataCollectorCmd -i $PREFIX -y getCpuMemCfg.sh  | grep -m 1 model | cut -d ':' -f 2`
+NUM_CPUS=`/home/jdd/WU-ARL/ndn-ops/NOC/nfdDataCollection/build/nfdDataCollectorCmd -i $PREFIX -y getCpuMemCfg.sh  | grep "processor" | wc -l`
+TOT_MEM=`/home/jdd/WU-ARL/ndn-ops/NOC/nfdDataCollection/build/nfdDataCollectorCmd -i $PREFIX -y getMemUsage.sh | cut -d ' ' -f 1`
 
-echo "$NODENAME"
-echo "UTC Time 	 PROC_SIZE 	 PIT_ENTRIES 	 NAME_TREE_ENTRIES"
+#echo "$NODENAME"
+NAME_AND_SPEC=`echo "$NODENAME:  $NUM_CPUS cpus of type $CPU_TYPE ;  total mem $TOT_MEM "`
+
+
+echo ""
+echo "$NAME_AND_SPEC"
+echo "        	  SYSTEM 	    NFD   	    NFD      	      NFD          "
+echo "UTC Time 	 FREE_MEM 	 PROC_SIZE 	 PIT_ENTRIES 	 NAME_TREE_ENTRIES "
 COUNT=0
 while (true)
 do
   DATE=`date +%s`
+  FREE_MEM=`/home/jdd/WU-ARL/ndn-ops/NOC/nfdDataCollection/build/nfdDataCollectorCmd -i $PREFIX -y getMemUsage.sh | cut -d ' ' -f 2`
   NFD_PROC_SIZE=`/home/jdd/WU-ARL/ndn-ops/NOC/nfdDataCollection/build/nfdDataCollectorCmd -i $PREFIX -y getNfdProcSize.sh`
   NFD_PIT_ENTRIES=`/home/jdd/WU-ARL/ndn-ops/NOC/nfdDataCollection/build/nfdDataCollectorCmd -i $PREFIX -y getPitEntries.sh`
   NFD_NAME_TREE_ENTRIES=`/home/jdd/WU-ARL/ndn-ops/NOC/nfdDataCollection/build/nfdDataCollectorCmd -i $PREFIX -y getNameTreeEntries.sh`
-  echo "$DATE 	 $NFD_PROC_SIZE 	 $NFD_PIT_ENTRIES 		 $NFD_NAME_TREE_ENTRIES"
+  echo "$DATE 	 $FREE_MEM 	 $NFD_PROC_SIZE 	 $NFD_PIT_ENTRIES 		 $NFD_NAME_TREE_ENTRIES"
 
   COUNT=$(($COUNT+1))
   if [ $COUNT -ge 8 ] 
   then
     COUNT=0
-    echo "$NODENAME"
-    echo "UTC Time 	 PROC_SIZE 	 PIT_ENTRIES 	 NAME_TREE_ENTRIES"
+    echo ""
+    echo "$NAME_AND_SPEC"
+    echo "        	  SYSTEM 	    NFD   	    NFD      	      NFD          "
+    echo "UTC Time 	 FREE_MEM 	 PROC_SIZE 	 PIT_ENTRIES 	 NAME_TREE_ENTRIES "
+    #echo "UTC Time 	 PROC_SIZE 	 PIT_ENTRIES 	 NAME_TREE_ENTRIES"
   fi
 sleep 10
 done
