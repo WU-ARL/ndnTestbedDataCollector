37f95bb45725cbe90712f2fb76f0db5b78f568d3
jdehart1 <jdd@wustl.edu>
2016-03-04 08:38:10 -0600

    Add interface scripts for rest of nodes

---
+++ scripts/getLink_p5p2_bw.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+TIME=`date +%s.%N`
+
+FIELDS=`ifconfig p5p2 | grep "RX bytes" | cut -d ':' -f 2,3`
+
+RX_BYTES=`echo $FIELDS | cut -d ' ' -f 1`
+TX_BYTES=`echo $FIELDS | cut -d ':' -f 2 | cut -d ' ' -f 1`
+
+echo "$TIME $RX_BYTES $TX_BYTES"
+
+
+
