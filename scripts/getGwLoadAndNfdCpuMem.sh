02ac7711fc01b9dd569b40f539cca9d144235b98
jdehart1 <jdd@wustl.edu>
2016-03-05 07:26:45 -0600

    add script for load average and nfd CPU and MEM percents

---
+++ scripts/getGwLoadAndNfdCpuMem.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+LOAD3=`w | grep "load average" | cut -d ','  -f 4-6 | cut -d ':' -f 2 | cut -d ',' --output-delimiter=' ' -f 1-3`
+#NFD_PID=`ps aux | grep nfd | grep config | tr -s ' ' | cut -d ' ' -f 2`
+#NFD_PID=`ps aux | grep nfd | grep config | tr -s ' ' | cut -d ' ' -f 2`
+#PS=`ps -p $NFD_PID -o comm,%cpu,%mem`
+
+
+NFD_CPU_MEM_PERCENTS=` ps aux | sed -n '/\/usr\/bin\/nfd /s/ \+/ /gp' | cut -d ' ' -f 3,4`
+#echo "$PS"
+echo "$LOAD3 $NFD_CPU_MEM_PERCENTS"
