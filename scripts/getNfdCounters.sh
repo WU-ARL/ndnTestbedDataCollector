0bfec1561568369170f6fca0bfc0571bce5b4f40
jdehart1 <jdd@wustl.edu>
2016-03-06 22:46:08 -0600

    add more coutners

--- scripts/getNfdCounters.sh
+++ scripts/getNfdCounters.sh
@@ -5,5 +5,7 @@ RESULTS=`nfd-status -v `
 NFD_PITENTRIES=`echo "$RESULTS" | grep Pit | cut -d'=' -f 2`
 NFD_NAMETREEENTRIES=`echo "$RESULTS" | grep NameTreeEntries | cut -d'=' -f 2`
 NFD_UPTIME=`echo "$RESULTS" | grep uptime | cut -d'=' -f 2`
-echo "pit:$NFD_PITENTRIES nametree:$NFD_NAMETREEENTRIES uptime:$NFD_UPTIME"
+NFD_FIBENTRIES=`echo "$RESULTS" | grep Fib | cut -d'=' -f 2`
+NFD_MEASUREMENTENTRIES=`echo "$RESULTS" | grep Measurements | cut -d'=' -f 2`
+echo "pit:$NFD_PITENTRIES nametree:$NFD_NAMETREEENTRIES uptime:$NFD_UPTIME fib:$NFD_FIBENTRIES measurements:$NFD_MEASUREMENTENTRIES"
 
