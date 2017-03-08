ed895688f254246c5513ce98cde988f677a9c10a
jdehart1 <jdd@wustl.edu>
2017-03-08 13:12:37 -0600

    change from nfd-status to nfdc

--- scripts/getNfdCounters.sh
+++ scripts/getNfdCounters.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #RESULTS=`nfd-status -v | grep Pit | cut -d'=' -f 2`
-RESULTS=`nfd-status -v `
+#RESULTS=`nfd-status -v `
+RESULTS=`nfdc status show`
 NFD_PITENTRIES=`echo "$RESULTS" | grep Pit | cut -d'=' -f 2`
 NFD_NAMETREEENTRIES=`echo "$RESULTS" | grep NameTreeEntries | cut -d'=' -f 2`
 NFD_UPTIME=`echo "$RESULTS" | grep uptime | cut -d'=' -f 2 | cut -d ' ' -f 1`
